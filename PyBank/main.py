6e6dc43f17f8c9d4f763e4601a7868131cb9432d
La'Berrick Williams <laberrick.williams@gmail.com>
2023-12-04 21:39:16 -0600

    Whats my name

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,88 +1,39 @@
-#Importing the necessary modules/files/variables
 import os
 import csv
-
-#Creating CSV file
-budget_data_csv = os.path.join("Resources", "budget_data.csv")
-
-total_months = 0
-total_pl = 0
-value = 0
-change = 0
-dates = []
-profits = []
-
-
-#Creating an object out of the CSV file
-budget_data = os.path.join("budget_data.csv")
-
-with open(budget_data_csv) as csvfile:
-    
-#Opening and reading the CSV file
-
-    csvreader = csv.reader(csvfile, delimiter = ",")
-
-    print(csvreader)
-    
-    for row in csvreader:
-        print(row)
-        
-        #Reading the header row
-    csv_header = next(csvreader)
-
-    #Reading the first row (so that we track the changes properly)
-    first_row = next(csvreader)
-    total_months += 1
-    total_pl += int(first_row[1])
-    value = int(first_row[1])
-    
-    #Going through each row of data after the header & first row 
-    for row in csvreader:
-        # Keeping track of the dates
-        dates.append(row[0])
-        
-        # Calculate the change, then add it to list of changes
-        change = int(row[1])-value
-        profits.append(change)
-        value = int(row[1])
-        
-        #Total number of months
-        total_months += 1
-
-        #Total net amount of "Profit/Losses over entire period"
-        total_pl = total_pl + int(row[1])
-
-    #Greatest increase in profits
-    greatest_increase = max(profits)
-    greatest_index = profits.index(greatest_increase)
-    greatest_date = dates[greatest_index]
-
-    #Greatest decrease (lowest increase) in profits 
-    greatest_decrease = min(profits)
-    worst_index = profits.index(greatest_decrease)
-    worst_date = dates[worst_index]
-
-    #Average change in "Profit/Losses between months over entire period"
-    avg_change = sum(profits)/len(profits)
-    
-
-#Displaying information
-print("Financial Analysis")
-print("---------------------")
-print(f"Total Months: {str(total_months)}")
-print(f"Total: ${str(total_pl)}")
-print(f"Average Change: ${str(round(avg_change,2))}")
-print(f"Greatest Increase in Profits: {greatest_date} (${str(greatest_increase)})")
-print(f"Greatest Decrease in Profits: {worst_date} (${str(greatest_decrease)})")
-
-#Exporing to .txt file
-output = open("output.txt", "w")
-
-line1 = "Financial Analysis"
-line2 = "---------------------"
-line3 = str(f"Total Months: {str(total_months)}")
-line4 = str(f"Total: ${str(total_pl)}")
-line5 = str(f"Average Change: ${str(round(avg_change,2))}")
-line6 = str(f"Greatest Increase in Profits: {greatest_date} (${str(greatest_increase)})")
-line7 = str(f"Greatest Decrease in Profits: {worst_date} (${str(greatest_decrease)})")
-output.write('{}\n{}\n{}\n{}\n{}\n{}\n{}\n'.format(line1,line2,line3,line4,line5,line6,line7))
+budgetData = os.path.join(".","Resources","budget_data.csv")
+with open(budgetData, newline="", encoding="utf-8") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    next(csvreader)
+    data = list(csvreader)
+    row_count = len(data)
+    total = 0
+    for i in range(0, row_count):
+        total = total + int(data[i][1])
+    num1 = 0
+    num2 = int(data[0][1])
+    diff = 0
+    difflist = list()
+    for j in range(1, row_count):
+        num1 = int(data[j][1])
+        diff = num1 - num2
+        difflist.append(diff)
+        num2 = int(data[j][1])
+    avgChange = round(sum(difflist)/len(difflist),2)
+    maxDiff = max(difflist)
+    maxDiffPos = difflist.index(maxDiff)+1
+    minDiff = min(difflist)
+    minDiffPos = difflist.index(minDiff)+1
+    print("Financial Analysis")
+    print("----------------------------")
+    print(f"Total Months: {row_count}")
+    print(f"Total: ${total:,}")
+    print(f"Average Change: ${avgChange:,}")
+    print(f"Greatest Increase in Profits: {data[maxDiffPos][0]} (${maxDiff:,})")
+    print(f"Greatest Decrease in Profits: {data[minDiffPos][0]} (${minDiff:,})")
+    print("Financial Analysis", file=open("PyBank.txt", "a"))
+    print("----------------------------", file=open("PyBank.txt", "a"))
+    print(f"Total Months: {row_count}", file=open("PyBank.txt", "a"))
+    print(f"Total: ${total:,}", file=open("PyBank.txt", "a"))
+    print(f"Average Change: ${avgChange:,}", file=open("PyBank.txt", "a"))
+    print(f"Greatest Increase in Profits: {data[maxDiffPos][0]} (${maxDiff:,})", file=open("PyBank.txt", "a"))
+    print(f"Greatest Decrease in Profits: {data[minDiffPos][0]} (${minDiff:,})", file=open("PyBank.txt", "a"))
